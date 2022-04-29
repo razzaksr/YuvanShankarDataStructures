27a20c56ceb2417df0b1645e39cb19c5a4648890
razzaksr <razzaksr@gmail.com>
2022-04-29 11:33:16 +0530

    Stack is over

--- YuvanShankarDS/src/main/java/data/structures/YuvanShankarDS/DemoStack.java
+++ YuvanShankarDS/src/main/java/data/structures/YuvanShankarDS/DemoStack.java
@@ -1,18 +1,43 @@
 package data.structures.YuvanShankarDS;
 
+import java.util.Scanner;
+
 public class DemoStack 
 {
 	public static void main(String[] args) 
 	{
 		StkPlan stk=new StkPlan();
-		
-		stk.traverse();
-		
-		stk.push("Yuvan");stk.push("Zealous");stk.push("Prakash");stk.push("Victor");
-		stk.push("Prem");stk.push("Vinayak");stk.push("Sabari");stk.push("Richard");
-		stk.push("Ravi");stk.push("Raj");stk.push("Sinivasan");stk.push("Snehan");
-		
-		stk.traverse();
+		Scanner scan=new Scanner(System.in);
+		String userOption="", userInput="";
+		do
+		{
+			System.out.println("Available options:\npush\npop\nsearch\npeek\ntraverse");
+			userOption=scan.next();
+			switch(userOption)
+			{
+			case "push":
+				System.out.println("Enter the data to push: ");
+				userInput=scan.next();
+				stk.push(userInput);
+				break;
+			case "pop":
+				stk.pop();
+				break;
+			case "search":
+				System.out.println("Enter the data to push: ");
+				userInput=scan.next();
+				stk.search(userInput);
+				break;
+			case "peek":
+				stk.peek();
+				break;
+			case "traverse":
+				stk.traverse();
+				break;
+			default:System.out.println("Invalid user option");
+			return;
+			}
+		}while(true);
 	}
 }
 
@@ -20,6 +45,50 @@ class StkPlan
 {
 	private String[] arr=new String[10];
 	private int top=-1;
+	
+	public void search(String data)
+	{
+		int dummyTop=top;
+		if(dummyTop==-1)
+		{
+			System.out.println("Stack is underflow");
+		}
+		else {
+			while(dummyTop>-1)
+			{
+				if(arr[dummyTop].equals(data))
+				{
+					System.out.println(dummyTop+" is the position where "+data+" located");
+					return;
+				}
+				dummyTop-=1;
+			}
+			System.out.println(data+" is not available in stack");
+		}
+	}
+	
+	public void peek()
+	{
+		if(top==-1)
+		{
+			System.out.println("Stack is underflow");
+		}
+		else {
+			System.out.println(arr[top]+" is peek element");
+		}
+	}
+	
+	public void pop()
+	{
+		if(top==-1)
+		{
+			System.out.println("Stack is underflow");
+		}
+		else {
+			System.out.println(arr[top]+" is popped");
+			top-=1;
+		}
+	}
 	
 	public void traverse()
 	{

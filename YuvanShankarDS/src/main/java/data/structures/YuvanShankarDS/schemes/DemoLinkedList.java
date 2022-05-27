fb5c4f8354c0fc5331d82f4cb64e7596cb0cfb68
razzaksr <razzaksr@gmail.com>
2022-05-27 11:08:28 +0530

    linked list add,remove done

--- YuvanShankarDS/src/main/java/data/structures/YuvanShankarDS/schemes/DemoLinkedList.java
+++ YuvanShankarDS/src/main/java/data/structures/YuvanShankarDS/schemes/DemoLinkedList.java
@@ -8,16 +8,106 @@ public class DemoLinkedList
 		ware.addAtEnd('R');ware.addAtEnd('A');ware.addAtEnd('S');ware.addAtEnd('T');
 		ware.addAtEnd('W');ware.addAtEnd('C');
 		ware.traverse();
+		ware.addAtBegin('Q');ware.addAtBegin('V');ware.addAtBegin('W');
+		ware.traverse();
+		ware.addAtAnyWhere(1, 'F');ware.addAtAnyWhere(5, 'K');
+		ware.traverse();
+		ware.removeAtBegin();ware.removeAtBegin();
+		ware.traverse();
+		ware.removeAtLast();ware.removeAtLast();ware.removeAtLast();
+		ware.traverse();
+		ware.removeAtAnyWhere(3);ware.removeAtAnyWhere(1);
+		ware.traverse();
 	}
 }
 
 
 class Warehouse
 {
-	private char[] list;
+	private char[] list; // 10>> length, 9>> length-1
 	private int head=-1;
+	//  
+	//       p   d  h
+	// 30 10 50 45 20
+	public void removeAtAnyWhere(int pos)
+	{
+		if(head==-1){
+			System.out.println("Nothing to remove at "+pos+" since list is underflow");
+		}
+		if(pos<=head) {
+			char old=list[pos];
+			int dummy=pos+1;
+			while(dummy<=head)
+			{
+				list[dummy-1]=list[dummy];
+				dummy++;
+			}
+			head--;
+			System.out.println(old+" has removed as it was in "+pos+" of the list");
+		}
+	}
 	
-	// newone 89 12 34 100
+	public void removeAtLast()
+	{
+		if(head==-1){
+			System.out.println("Nothing to remove at last since list is underflow");
+		}
+		else {
+			head=head-1;
+			System.out.println(list[head+1]+" has removed from list as remove at last requested");
+		}
+	}
+	
+	// 30  10 8  12 35 20
+	// 	   d		   d  h
+	// 100 30 10 8 12 35 20
+	public void removeAtBegin()
+	{
+		if(head==-1)
+		{
+			System.out.println("Nothing to remove since list is underflow");
+		}
+		else {
+			char old=list[0];
+			int dummy=1;
+			while(dummy<=head)
+			{
+				list[dummy-1]=list[dummy];
+				dummy++;
+			}
+			head--;
+			System.out.println(old+" has removed as it was in beginning of the list");
+		}
+	}
+	
+	//             p
+	// 0  1  2  3  4  5
+	// 10 30 10 70 3 100
+	public void addAtAnyWhere(int pos,char data)
+	{
+		// underflow
+		if(head==-1){
+			System.out.println("List is underflow");
+			list[0]=data;head++;
+			System.out.println(data+" has inserted @ 0 not in "+pos);
+		}
+		if(pos<=list.length-2){
+			int dummy=head;
+			
+			while(dummy>=pos) {
+				list[dummy+1]=list[dummy];
+				dummy--;
+			}
+			list[pos]=data;head++;
+			System.out.println(data+" has inserted in linked list @ "+pos);
+		}
+		else {
+			System.out.println("Invalid position "+pos);
+		}
+	}
+	
+	//	list  d	  d	d	d  d+1
+	// newone new 89 12 34 100
 	public void addAtBegin(char data)
 	{
 		if(head==-1){
@@ -25,7 +115,18 @@ class Warehouse
 			list[0]=data;head++;
 		}
 		else {
-			
+			int dummy=head;
+			if( dummy+1 <= list.length-1 ){
+				while(dummy>=0){
+					list[dummy+1]=list[dummy];
+					dummy--;
+				}
+				list[0]=data;head++;
+				System.out.println(data+" has added @ beginning of the linked list");
+			}
+			else {
+				System.out.println("Add @ begin will faild due to overflow");
+			}
 		}
 	}
 	

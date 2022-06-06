2f4479afa1e88a5be6e92e7645ebd5795cfe4757
razzaksr <razzaksr@gmail.com>
2022-06-06 11:13:48 +0530

    Queue done

--- YuvanShankarDS/src/main/java/data/structures/YuvanShankarDS/schemes/DemoQueue.java
+++ YuvanShankarDS/src/main/java/data/structures/YuvanShankarDS/schemes/DemoQueue.java
@@ -9,6 +9,23 @@ public class DemoQueue
 		line.enqueue(32.3F);line.enqueue(8.3F);line.enqueue(2F);
 		line.enqueue(8.1F);line.enqueue(17.4F);
 		line.iterate();
+		
+		System.out.println(line.dequeue()+" has deleted");
+		System.out.println(line.dequeue()+" has deleted");
+		
+		System.out.println(line.linear(17.4F));
+		System.out.println(line.linear(12.3F));
+		System.out.println(line.linear(32.3F));
+		
+		System.out.println(line.readByPosition(3));
+		System.out.println(line.readByPosition(10));
+		
+		System.out.println(line.readFront());
+		System.out.println(line.readRear());
+		
+		line.iterate();
+		line.quick(line.getRear(), line.getFront());
+		line.iterate();
 	}
 }
 
@@ -17,15 +34,98 @@ class Line
 	private float[] arr;
 	private int frontEnd=-1,rearBegin=-1;
 	
+	public int getRear() {
+		return rearBegin;
+	}
+	
+	public int getFront() {
+		return frontEnd;
+	}
+	
+	public int sort(int start,int end)
+	{
+		float piData=arr[end];
+		int init=start-1,cur;
+		
+		for(cur=start;cur<end;cur++)
+		{
+			if(arr[cur]<piData)// swapping decided
+			{
+				init++;
+				float tmp=arr[cur];
+				arr[cur]=arr[init];
+				arr[init]=tmp;
+			}
+		}
+		
+		float tmp=arr[init+1];
+		arr[init+1]=arr[end];
+		arr[end]=tmp;
+		
+		return init+1;
+		
+	}
+	
+	public void quick(int start,int end)
+	{
+		if(end>start)
+		{
+			int piPoint=sort(start,end);
+			quick(start, piPoint-1);
+			quick(piPoint+1,end);
+		}
+	}
+	
+	public float readRear(){
+		return arr[rearBegin];
+	}
+	
+	public float readFront() {
+		return arr[frontEnd];
+	}
+	
+	public float readByPosition(int pos)
+	{
+		if(pos>=rearBegin&&pos<=frontEnd)
+			return arr[pos];
+		else
+			return 0.0F;
+	}
+	
+	public int linear(float data)
+	{
+		int dummy=frontEnd;
+		while(dummy>=rearBegin)
+		{
+			if(data==arr[dummy])
+				return dummy;
+			dummy--;
+		}
+		return -1;
+	}
+	
+	public float dequeue()
+	{
+		if(frontEnd==-1) {
+			System.out.println("Queue is underflow");
+			return -1;
+		}
+		else {
+			frontEnd--;
+			return arr[frontEnd+1];
+		}
+	}
+	
 	public void iterate()
 	{
-		System.out.println("Iterating all the queue elements");
+		System.out.println("Iterating all the queue elements: ");
 		int dummy=rearBegin;
 		while(dummy<=frontEnd)
 		{
-			System.out.println(arr[dummy]);
+			System.out.print(arr[dummy]+" ");
 			dummy++;
 		}
+		System.out.println();
 	}
 	
 	public void enqueue(float data)

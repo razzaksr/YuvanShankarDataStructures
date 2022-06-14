877e7e3d6c8fc94ef82b022bf91daa3fc2edbe94
razzaksr <razzaksr@gmail.com>
2022-06-14 10:31:51 +0530

    Binary Tree using Array over

--- YuvanShankarDS/src/main/java/data/structures/YuvanShankarDS/schemes/DemoBinaryTreeAsArray.java
+++ YuvanShankarDS/src/main/java/data/structures/YuvanShankarDS/schemes/DemoBinaryTreeAsArray.java
@@ -20,6 +20,13 @@ public class DemoBinaryTreeAsArray{
 		bin.preOrder(0);
 		System.out.println();
 		bin.postOrder(0);
+		
+		bin.callBFS();
+		
+		System.out.println("searching for JavaSE "+bin.linear(0, "JavaSE"));
+		System.out.println("searching for Uvan Shankar "+bin.linear(0, "Uvan Shankar"));
+		System.out.println("searching for Python "+bin.linear(0, "Python"));
+		System.out.println("searching for CPP "+bin.linear(0, "CPP"));
 	}
 }
 
@@ -28,6 +35,55 @@ public class DemoBinaryTreeAsArray{
 class BinArrayTree
 {
 	private String[] arr;
+	
+	public int linear(int root,String data)
+	{
+		if(root>=arr.length||arr[root]==null)
+			return -1;
+		if(arr[root].equals(data))
+			return root;
+		int left=linear(root*2+1,data);
+		if(left!=-1)
+			return left;
+		else
+			return linear(root*2+2,data);
+	}
+	
+	public void callBFS()
+	{
+		System.out.println("BFS Traversal");
+		int ht=seekHeight(0);
+		for(int ind=1;ind<=ht;ind++)
+			bfsTraverse(0, ind);
+		System.out.println();
+	}
+	
+	private void bfsTraverse(int root,int level)
+	{
+		if(root>=arr.length||arr[root]==null)
+			return;
+		else if(level==1)
+			System.out.print(arr[root]+" ");
+		else {
+			bfsTraverse(root*2+1, level-1);// level-=1 or level-- level=level-1
+			bfsTraverse(root*2+2, level-1);
+		}
+	}
+	
+	private int seekHeight(int root)
+	{
+		if(root>=arr.length||arr[root]==null)
+			return 0;
+		else {
+			int left=seekHeight(root*2+1);
+			int right=seekHeight(root*2+2);
+			
+			if(left>right)
+				return left+1;
+			else
+				return right+1;
+		}
+	}
 	
 	public void postOrder(int root)	
 	{
